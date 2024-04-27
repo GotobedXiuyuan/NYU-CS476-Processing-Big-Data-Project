8a5b8a0e91b54bd300a2b8bc02024ce9ce6990ca
GotobedXiuyuan <78220045+GotobedXiuyuan@users.noreply.github.com>
2024-04-27 14:30:46 -0400

    Update analytics.scala

--- ana_code/analytics.scala
+++ ana_code/analytics.scala
@@ -7,21 +7,21 @@ val spark = SparkSession.builder()
 
 import spark.implicits._
 
-val cleanedDF = spark.read.option("header", "true").csv("new4.csv")
+val cleaned = spark.read.option("header", "true").csv("new4.csv")
 
-val monthlyArrestsDF = cleanedDF
+val monthlyArrestsTrend = cleaned
   .withColumn("ARREST_MONTH", expr("substring(ARREST_DATE, 0, 2)"))
   .groupBy("ARREST_MONTH")
   .count()
   .orderBy("ARREST_MONTH")
 
-val severityDistributionDF = cleanedDF
+val severityDistributionForBorough = cleaned
   .groupBy("ARREST_BORO", "LAW_CAT_CD")
   .count()
   .orderBy("ARREST_BORO", "LAW_CAT_CD")
 
-monthlyArrestsDF.show()
-severityDistributionDF.show()
+monthlyArrestsTrend.show()
+severityDistributionForBorough.show()
 
 spark.stop()
 
